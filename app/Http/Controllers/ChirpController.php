f1f9246084f399cae83bfc065fc79894b7566133
jed-parsec <edulanjomari32@gmail.com>
2023-02-19 23:29:08 +0800

    implemented delete chirp functionality

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -81,6 +81,10 @@ class ChirpController extends Controller
      */
     public function destroy(Chirp $chirp): RedirectResponse
     {
-        //
+        $this->authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        return redirect(route('chirps.index'));
     }
 }
